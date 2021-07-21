6927685c6e676c84a8586b6455d353cf48d84c95
Andrés Pujol <andrespujol@gmail.com>
2021-07-20 23:23:38 -0300

    agregué navBar

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar';
+
+export const Header = () => {
+    return (
+        <>
+        <h1>Morena</h1>
+        <NavBar />
+        </>
+    )
+}
+
+export default Header;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <p><a href="index.html">Home</a></p>
+            <p><a href="index.html">Productos</a></p>
+            <p><a href="index.html">Sobre nosotros</a></p>
+        </div>
+    )
+}
+export default NavBar;
